0bd23b29d4843722ae2797e0a5ac4fbabe07fbbe
Ömercan Baylam <kurumsalomer0@gmail.com>
2023-02-13 12:17:01 +0300

    Spellings corrected and translated into English

--- son-depremler-telegram-bot.py
+++ son-depremler-telegram-bot.py
@@ -4,23 +4,32 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
+#Data[0] --> Date
+#Data[1] --> Latitude
+#Data[2] --> Longitude
+#Data[3] --> Depth
+#Data[4] --> Type
+#Data[5] --> Size
+#Data[6] --> Location
+#Data[7] --> ID
 
-bot_id="bottoken_buraya_yazıacak"
-chat_id_log= "log_kanalı_chatid_buraya_yazılacak"
-chat_id_news="chatid_buraya_yazılacak" # İsteğe bağlı kullanılmayacaksa sendreport() fonksiyonu ve çağırıldığı yerler silinmeli.
+bot_id="<bot_token>"
+chat_id_log= "<chatid>"
+chat_id_news="<log_channel_chatid>" # chat_id_news is optional. Is the log channel
 
+def getData(): # Get data from url
 
-def getData(): #son datayı getir
-
+    # Converting the data we access from the internet to python
     url = "https://deprem.afad.gov.tr/last-earthquakes.html"
     page = requests.get(url)    
     soup = BeautifulSoup(page.content, "html.parser")
 
-    # Verileri bul
+    # Find the datas
     data = []
 
     table = soup.find("table")
     rows = table.find_all("tr")
+
     for row in rows:
         cols = row.find_all("td")
         cols = [ele.text.strip() for ele in cols]
@@ -28,21 +37,13 @@ def getData(): #son datayı getir
 
     return data[1]
 
-#Data[0] --> Tarih
-#Data[1] --> Enlem
-#Data[2] --> Boylam
-#Data[3] --> Derinlik
-#Data[4] --> Tip
-#Data[5] --> Büyüklük
-#Data[6] --> Konum
-#Data[7] --> ID
 
+def sendMessage(data): # Send message to Telegram
 
-def sendMessage(data):
-
+    # The content of the message to be sent
     text=f"· Yer: {data[6]}\n· Büyüklük: {data[5]}\n· Tarih: {data[0]}"
     try:
-
+        # Telegram API connection
         send = requests.get(
             f'https://api.telegram.org/bot{bot_id}/sendMessage?chat_id={chat_id_news}&text=' + str(
                 text), timeout=2)
@@ -50,27 +51,29 @@ def sendMessage(data):
         if send.status_code > 299:
             print('TELEGRAM_ALARM_PUSH_ERROR: ', send.text)
         else:
-            print('Mesaj Başarıyla Gönderildi.')
+            print('Message sent successfully.')
 
     except Exception as exc:
         print('telegram push error: ' + str(exc))
 
-def sendReport():
+def sendReport(): # Send message to Telegram log channel. This function can be deleted if the log channel is not used
     text="working now time: " + str(datetime.datetime.now())
     try:
+        # Telegram API connection
         send = requests.get(f'https://api.telegram.org/bot{bot_id}/sendMessage?chat_id={chat_id_log}&text=' + text, timeout=2)
 
         if send.status_code > 299:
             print('TELEGRAM_ALARM_PUSH_ERROR: ', send.text)
         else:
-            print('Mesaj Başarıyla Gönderildi.')
+            print('Message sent successfully.')
 
     except Exception as exc:
         print('telegram push error: ' + str(exc))
 
 
-
-lastMessage=544856
+# It creates and saves the ID so that it does not send the last message again
+lastMessage=0
+# Sends notification when this number is a multiple of 6 (line:87)
 reportNum=0
 
 while True:
@@ -81,7 +84,7 @@ while True:
             lastMessage=data[-1]
             sendMessage(data)
 
-    if reportNum%60==0: # 5* bu sayı kadar saniye
+    if reportNum%60==0:
         sendReport()
         reportNum=0
         
